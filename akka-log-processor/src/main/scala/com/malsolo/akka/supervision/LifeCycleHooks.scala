fb647591dcd017e96ffe7d6d8aeb9c943322464f
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-12-22 01:01:40 +0100

    akka-katas: akka-log-processor. Actor for LifeCycleHooks: test and main

--- akka-log-processor/src/main/scala/com/malsolo/akka/supervision/LifeCycleHooks.scala
+++ akka-log-processor/src/main/scala/com/malsolo/akka/supervision/LifeCycleHooks.scala
@@ -1,6 +1,9 @@
 package com.malsolo.akka.supervision
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.util.Timeout
+
+import scala.util.{Failure, Success}
 
 class LifeCycleHooks extends Actor with ActorLogging {
   override def receive = {
@@ -8,4 +11,26 @@ class LifeCycleHooks extends Actor with ActorLogging {
       log.info(s"Received message $msg, sending it back")
       sender() ! msg
   }
+}
+
+object LifeCycleHooksMain extends App {
+
+  import scala.concurrent.duration._
+  import akka.pattern.ask
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val system = ActorSystem("LifeCycleHooks")
+  val lifeCycleHooks = system.actorOf(Props[LifeCycleHooks], "lifeCycleHooks")
+
+  implicit val timeout = Timeout(1 second)
+  val future = lifeCycleHooks ? "HELLO"
+  future.onComplete {
+    case Success(msg) => println(s"Received back $msg")
+    case Failure(e) => e printStackTrace
+  }
+
+  Thread.sleep(100)
+
+  system.terminate()
+
 }
---
+++ akka-log-processor/src/test/scala/com/malsolo/akka/supervision/LifeCycleHooksTest.scala
@@ -0,0 +1,25 @@
+package com.malsolo.akka.supervision
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.TestKit
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+class LifeCycleHooksTest extends TestKit(ActorSystem("LifeCycleHooksTest")) with WordSpecLike with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    system terminate
+  }
+
+  "The Child" must {
+    "log lifecycle hooks" in {
+      val testActorRef = system.actorOf(Props[LifeCycleHooks], "LifeCycleHooks")
+      watch(testActorRef)
+      //testActorRef ! "hello"
+      testActorRef.tell("hello", testActor)
+      expectMsg("hello")
+      system.stop(testActorRef)
+      expectTerminated(testActorRef)
+    }
+  }
+
+}
