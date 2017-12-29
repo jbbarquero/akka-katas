8b377b6ce7bb71c54dfb48c6c62546a8d95888f0
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-12-29 11:24:01 +0100

    akka-katas: akka-streaming. GenerateLogFile app for creating test log files.

---
+++ akka-streaming/src/main/scala/com/malsolo/akka/streaming/GenerateLogFile.scala
@@ -0,0 +1,59 @@
+package com.malsolo.akka.streaming
+
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption._
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{FileIO, Keep, Source}
+import akka.util.ByteString
+
+object GenerateLogFile extends App {
+
+  if (args.length != 2) {
+    System.err.print("Usage. Provide two args: file-path number-of-lines")
+    System.exit(1)
+  }
+
+  val filePath = args(0)
+  val numberOfLines = args(1).toInt
+  val rnd = new java.util.Random()
+
+  val sink = FileIO.toPath(Paths.get(filePath), Set(CREATE, WRITE, APPEND))
+
+  def hostname() = {
+    val os = System.getProperty("os.name").toLowerCase
+
+    if (os.indexOf("win") >= 0) System.getenv("COMPUTERNAME") else System.getenv("HOSTNAME")
+  }
+
+  def line(i: Int) = {
+    val host = hostname()
+    val time = ZonedDateTime.now.format(DateTimeFormatter.ISO_INSTANT)
+    val state = if (i % 10 == 0) "warining"
+      else if (i % 101 == 0) "error"
+      else if (i% 1002 == 0) "critical"
+      else "ok"
+    val description = "description"
+    val tag = "tag"
+    val metric = rnd.nextDouble() * 100
+
+    s"$host | $time | $state | $time | $description | $tag | $metric \n"
+  }
+
+  val graph = Source.fromIterator { () =>
+    Iterator.tabulate(numberOfLines)(line)
+  }.map(l => ByteString(1)).toMat(sink)(Keep.right)
+
+  implicit val system = ActorSystem()
+  implicit val ec = system.dispatcher
+  implicit val materializer = ActorMaterializer()
+
+  graph.run().foreach { result =>
+    println(s"Wrote ${result.count} bytes to $filePath")
+    system.terminate()
+  }
+
+}
