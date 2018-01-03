1fb1c09ea4a3c575475d26fc7a29e9ca94bf9226
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-01-03 09:49:22 +0100

    akka-katas: akka-quickstart-scala. Minor fixes: formatting.

--- akka-quickstart-scala/src/main/scala/com/malsolo/akka/quickstart/scala/AkkaQuickstart.scala
+++ akka-quickstart-scala/src/main/scala/com/malsolo/akka/quickstart/scala/AkkaQuickstart.scala
@@ -3,12 +3,14 @@ package com.malsolo.akka.quickstart.scala
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
 object Printer {
+
   final case class Greeting(greeting: String)
 
   def props(): Props = Props[Printer]
 }
 
 class Printer extends Actor with ActorLogging {
+
   import Printer._
 
   override def receive = {
@@ -18,13 +20,16 @@ class Printer extends Actor with ActorLogging {
 }
 
 object Greeter {
+
   final case class WhoToGreet(who: String)
+
   case object Greet
 
   def props(message: String, printerActor: ActorRef): Props = Props(new Greeter(message, printerActor))
 }
 
 class Greeter(message: String, printerActor: ActorRef) extends Actor {
+
   import Greeter._
   import Printer._
 
