8baed10f529f088398b9257cceabd73aa311f436
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-01-03 06:36:04 +0100

    akka-katas: akka-streaming. Flows: logic for parsing log lines

---
+++ akka-streaming/src/main/scala/com/malsolo/akka/streaming/flows/LogStreamProcessor.scala
@@ -0,0 +1,47 @@
+package com.malsolo.akka.streaming.flows
+
+import java.time.ZonedDateTime
+
+object LogStreamProcessor extends EventMarshalling {
+
+  /**
+    * parses text log line into an Event
+    */
+  def parseLineEx(line: String): Option[Event] = {
+    if(!line.isEmpty) {
+      line.split("\\|") match {
+        case Array(host, service, state, time, desc, tag, metric) =>
+          val t = tag.trim
+          val m = metric.trim
+          Some(Event(
+            host.trim,
+            service.trim,
+            state.trim match {
+              case State(s) => s
+              case _        => throw new Exception(s"Unexpected state: $line")
+            },
+            ZonedDateTime.parse(time.trim),
+            desc.trim,
+            if(t.nonEmpty) Some(t) else None,
+            if(m.nonEmpty) Some(m.toDouble) else None
+          ))
+        case Array(host, service, state, time, desc) =>
+          Some(Event(
+            host.trim,
+            service.trim,
+            state.trim match {
+              case State(s) => s
+              case _        => throw new Exception(s"Unexpected state: $line")
+            },
+            ZonedDateTime.parse(time.trim),
+            desc.trim
+          ))
+        case x =>
+          throw new LogParseException(s"Failed on line: $line")
+      }
+    } else None
+  }
+
+  case class LogParseException(msg:String) extends Exception(msg)
+
+}
