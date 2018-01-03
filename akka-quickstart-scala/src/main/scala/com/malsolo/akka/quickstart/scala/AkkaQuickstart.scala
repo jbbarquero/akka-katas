b39581a0d3f4f1bb0d1f7494f1122459ee32ff17
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-01-03 09:41:38 +0100

    akka-katas: akka-quickstart-scala. Greeter actor skeleton.
    · With its companion object

--- akka-quickstart-scala/src/main/scala/com/malsolo/akka/quickstart/scala/AkkaQuickstart.scala
+++ akka-quickstart-scala/src/main/scala/com/malsolo/akka/quickstart/scala/AkkaQuickstart.scala
@@ -1,5 +1,18 @@
 package com.malsolo.akka.quickstart.scala
 
+import akka.actor.{Actor, ActorRef, Props}
+
+object Greeter {
+  final case class WhoToGreet(who: String)
+  case object Greet
+
+  def props(message: String, printerActor: ActorRef): Props = Props(new Greeter(message, printerActor))
+}
+
+class Greeter(message: String, printerActor: ActorRef) extends Actor {
+  override def receive: Receive = ???
+}
+
 object AkkaQuickstart extends App {
 
 }
