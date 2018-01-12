d46e2fb526c5959ebd067acc88314d6386455f4c
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-01-12 18:16:17 +0100

    akka-katas: akka-example. Device actor with write messages
    See https://doc.akka.io/docs/akka/current/guide/tutorial_3.html#actor-with-read-and-write-messages

--- akka-example/src/main/scala/com/malsolo/akka/sample/scala/Device.scala
+++ akka-example/src/main/scala/com/malsolo/akka/sample/scala/Device.scala
@@ -5,6 +5,9 @@ import akka.actor.{Actor, ActorLogging, Props}
 object Device {
   def props(groupId: String, deviceId: String): Props = Props(new Device(groupId, deviceId))
 
+  final case class RecordTemperature(requestId: Long, value: Double)
+  final case class TemperatureRecorded(requestId: Long)
+
   final case class ReadTemperature(requestId: Long)
   final case class RespondTemperature(requesId: Long, value: Option[Double])
 }
@@ -15,7 +18,16 @@ class Device(groupId: String, deviceId: String) extends Actor with ActorLogging 
 
   var lastTemperaturaReading: Option[Double] = None
 
+  override def preStart(): Unit = log.info("Device actor {}-{} started", groupId, deviceId)
+
+  override def postStop(): Unit = log.info("Device actor {}-{} stopped", groupId, deviceId)
+
   override def receive: Receive = {
+    case RecordTemperature(id, value) =>
+      log.info("Recorded temperature reading {} with {}", value, id)
+      lastTemperaturaReading = Some(value)
+      sender() ! TemperatureRecorded(id)
+
     case ReadTemperature(id) =>
       sender() ! RespondTemperature(id, lastTemperaturaReading)
   }
