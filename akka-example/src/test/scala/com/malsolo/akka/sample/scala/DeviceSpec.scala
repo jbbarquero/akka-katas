6e321144a86f36a3a23707840f07939191fb3ca7
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-01-15 07:55:39 +0100

    akka-katas: akka-example. Testing writing messages for Device actor

--- akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceSpec.scala
+++ akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceSpec.scala
@@ -17,8 +17,28 @@ class DeviceSpec extends TestKit(ActorSystem("device-test-system"))
     val response = probe.expectMsgType[Device.RespondTemperature]
     response.requesId must be (42)
     response.value must be (None)
+  }
 
+  "response with temperature recorded" in {
+    val probe = TestProbe()
+    val deviceActor = system.actorOf(Device.props("group", "device"))
 
+    deviceActor.tell(Device.RecordTemperature(41, 30), probe.ref)
+    val response = probe.expectMsgType[Device.TemperatureRecorded]
+    response.requestId must be (41)
+  }
+
+  "reply with last temperature reading" in {
+    val probe = TestProbe()
+    val deviceActor = system.actorOf(Device.props("group", "device"))
+
+    deviceActor.tell(Device.RecordTemperature(1, 24.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(1))
+
+    deviceActor.tell(Device.ReadTemperature(2), probe.ref)
+    val response1 = probe.expectMsgType[Device.RespondTemperature]
+    response1.requesId must be (2)
+    response1.value must be (Some(24.0))
   }
 
 }
