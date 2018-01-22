d9af3eb6f6e05a2678306968b9126b2116853a6b
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-01-22 11:45:19 +0100

    akka-katas: akka-example. Device group registration, implementation finished.

--- akka-example/src/main/scala/com/malsolo/akka/sample/scala/DeviceGroup.scala
+++ akka-example/src/main/scala/com/malsolo/akka/sample/scala/DeviceGroup.scala
@@ -1,11 +1,34 @@
 package com.malsolo.akka.sample.scala
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import com.malsolo.akka.sample.scala.DeviceManager.IncorrectRequestTrackDeviceForGroup
+import com.malsolo.akka.sample.scala.DeviceManager.RequrestTrackDevice
 
 object DeviceGroup {
   def props(groupId: String): Props = Props(new DeviceGroup(groupId))
 }
 
 class DeviceGroup(groupId: String) extends Actor with ActorLogging {
-  override def receive: Receive = ???
+
+  var deviceIdToActor = Map.empty[String, ActorRef]
+
+  override def preStart(): Unit = log.info("Device group {} started", groupId)
+
+  override def postStop(): Unit = log.info("Device group {} stopped", groupId)
+
+  override def receive: Receive = {
+    case trackMsg @ RequrestTrackDevice(`groupId`, _) =>
+      deviceIdToActor.get(trackMsg.deviceId) match {
+        case Some(deviceActor) =>
+          deviceActor forward trackMsg
+        case None =>
+          log.info("Creating device actor for {}", trackMsg.deviceId)
+          val deviceACtor = context.actorOf(Device.props(groupId, trackMsg.deviceId), s"device-${trackMsg.deviceId}")
+          deviceIdToActor += trackMsg.deviceId -> deviceACtor
+          deviceACtor forward trackMsg
+      }
+    case RequrestTrackDevice(groupId, deviceId) =>
+      log.warning("Ignoring TrackDevice request for {}. This actor is responsible for {}.", groupId, this.groupId)
+      sender() ! IncorrectRequestTrackDeviceForGroup(IncorrectGroupId = groupId, managedGroupId = this.groupId)
+  }
 }
