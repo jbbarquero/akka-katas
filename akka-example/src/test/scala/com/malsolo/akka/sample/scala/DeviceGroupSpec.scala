441d589a6e3cd3aafd1bfde703113fd7557b55c7
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-01-26 17:44:57 +0100

    akka-katas: akka-example. Device groups, test to ensure that the device ID is properly removed after the device actor has been stopped:. FAILING!
    See https://doc.akka.io/docs/akka/current/guide/tutorial_4.html#keeping-track-of-the-device-actors-in-the-group

--- akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupSpec.scala
+++ akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupSpec.scala
@@ -1,6 +1,6 @@
 package com.malsolo.akka.sample.scala
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, PoisonPill}
 import akka.testkit.{TestKit, TestProbe}
 import org.scalatest.{MustMatchers, WordSpecLike}
 
@@ -65,6 +65,40 @@ class DeviceGroupSpec extends TestKit(ActorSystem("devicegroup-test-system"))
 
     //THEN
     probe.expectMsg(DeviceGroup.ReplyDeviceList(1, Set("device1", "device2")))
+  }
+
+  "be able to list active devices after one shuts down" in {
+    //GIVEN
+    val probe = TestProbe()
+    val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+    //AND
+    groupActor.tell(DeviceManager.RequrestTrackDevice("group", "device1"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered("group", "device1"))
+    val toShutDown = probe.lastSender
+
+    groupActor.tell(DeviceManager.RequrestTrackDevice("group", "device2"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered("group", "device2"))
+
+    //WHEN
+    groupActor.tell(DeviceGroup.RequestDeviceList(0), probe.ref)
+
+    //THEN
+    probe.expectMsg(DeviceGroup.ReplyDeviceList(0, Set("device1", "device2")))
+
+    //AND WHEN
+    probe.watch(toShutDown)
+    toShutDown ! PoisonPill
+    probe.expectTerminated(toShutDown)
+
+    Thread.sleep(2000)
+
+    //THEN
+    probe.awaitAssert {
+      groupActor.tell(DeviceGroup.RequestDeviceList(1), probe.ref)
+      probe.expectMsg(DeviceGroup.ReplyDeviceList(1, Set("device2")))
+    }
+
   }
 
 }
