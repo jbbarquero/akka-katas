542991a99757668b4d5fd6a7c5850014d096950a
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-02-22 01:18:06 +0100

    akka-katas: akka-example. Testing the query actor. Try out the case when there are two devices and both report a temperature.

--- akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupQuerySpec.scala
+++ akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupQuerySpec.scala
@@ -1,7 +1,9 @@
 package com.malsolo.akka.sample.scala
 
+import scala.concurrent.duration._
+
 import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.testkit.{TestKit, TestProbe}
 import org.scalatest.{MustMatchers, WordSpecLike}
 
 class DeviceGroupQuerySpec extends TestKit(ActorSystem("devicegroupquery-test-system"))
@@ -9,6 +11,32 @@ class DeviceGroupQuerySpec extends TestKit(ActorSystem("devicegroupquery-test-sy
   with MustMatchers
   with StopSystemAfterAll {
 
+  "return temperature value for working devices" in {
+    val requester = TestProbe()
 
+    val device1 = TestProbe()
+    val device2 = TestProbe()
+
+    val queryActor = system.actorOf(DeviceGroupQuery.props(
+      actorToDeviceId = Map(device1.ref -> "device1", device2.ref -> "device2"),
+      requestId = 1,
+      requester = requester.ref,
+      timeout = 3 seconds
+    ))
+
+    device1.expectMsg(Device.ReadTemperature(requestId = 0))
+    device2.expectMsg(Device.ReadTemperature(requestId = 0))
+
+    queryActor.tell(Device.RespondTemperature(requestId = 0, Some(1.0)), device1.ref)
+    queryActor.tell(Device.RespondTemperature(requestId = 0, Some(1.0)), device2.ref)
+
+    requester.expectMsg(DeviceGroup.RespondAllTemperatures(
+      requestId = 1,
+      temperatures = Map(
+        "device1" -> DeviceGroup.Temperature(1.0),
+        "device2" -> DeviceGroup.Temperature(2.0)
+      )
+    ))
+  }
 
 }
