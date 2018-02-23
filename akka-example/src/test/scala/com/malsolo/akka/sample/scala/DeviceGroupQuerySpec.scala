bfb6b2c685a6b334669cfd45fed4168d1a7fd4a2
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-02-23 07:57:24 +0100

    akka-katas: akka-example. Testing the query actor. Try out the case when some device cannot provide a temperature measurement.

--- akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupQuerySpec.scala
+++ akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupQuerySpec.scala
@@ -39,4 +39,32 @@ class DeviceGroupQuerySpec extends TestKit(ActorSystem("devicegroupquery-test-sy
     ))
   }
 
+  "return TemperatureNotAvailable for devices with no readings" in {
+    val requester = TestProbe()
+
+    val device1 = TestProbe()
+    val device2 = TestProbe()
+
+    val queryActor = system.actorOf(DeviceGroupQuery.props(
+      actorToDeviceId = Map(device1.ref -> "device1", device2.ref -> "device2"),
+      requestId = 1,
+      requester = requester.ref,
+      timeout = 3 seconds
+    ))
+
+    device1.expectMsg(Device.ReadTemperature(requestId = 0))
+    device2.expectMsg(Device.ReadTemperature(requestId = 0))
+
+    queryActor.tell(Device.RespondTemperature(requestId = 0, None), device1.ref)
+    queryActor.tell(Device.RespondTemperature(requestId = 0, Some(2.0)), device2.ref)
+
+    requester.expectMsg(DeviceGroup.RespondAllTemperatures(
+      requestId = 1,
+      temperatures = Map(
+        "device1" -> DeviceGroup.TemperatureNotAvailable,
+        "device2" -> DeviceGroup.Temperature(2.0)
+      )
+    ))
+  }
+
 }
