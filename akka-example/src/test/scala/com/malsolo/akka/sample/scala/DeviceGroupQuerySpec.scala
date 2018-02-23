0f9f43c1767ce6073513f72a382fa30ca15ca793
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-02-23 18:32:14 +0100

    akka-katas: akka-example. Try out the case when not all devices respond in time.
    Note: I had to put a duration for the expected message, otherwise, sometimes it failed

--- akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupQuerySpec.scala
+++ akka-example/src/test/scala/com/malsolo/akka/sample/scala/DeviceGroupQuerySpec.scala
@@ -124,4 +124,34 @@ class DeviceGroupQuerySpec extends TestKit(ActorSystem("devicegroupquery-test-sy
     ))
   }
 
+  "return DeviceTimedOut if device does not answer in time" in {
+    val requester = TestProbe()
+
+    val device1 = TestProbe()
+    val device2 = TestProbe()
+
+    val queryActor = system.actorOf(DeviceGroupQuery.props(
+      actorToDeviceId = Map(device1.ref -> "device1", device2.ref -> "device2"),
+      requestId = 1,
+      requester = requester.ref,
+      timeout = 3 seconds
+    ))
+
+    device1.expectMsg(Device.ReadTemperature(requestId = 0))
+    device2.expectMsg(Device.ReadTemperature(requestId = 0))
+
+    queryActor.tell(Device.RespondTemperature(requestId = 0, Some(1.0)), device1.ref)
+
+    requester.expectMsg(
+      4 seconds,
+      DeviceGroup.RespondAllTemperatures(
+        requestId = 1,
+        temperatures = Map(
+          "device1" -> DeviceGroup.Temperature(1.0),
+          "device2" -> DeviceGroup.DeviceTimedOut
+        )
+      )
+    )
+  }
+
 }
